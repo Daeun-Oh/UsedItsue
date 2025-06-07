ee228d12cd622bdc2d02e4ebce9586ec620df9ce
Daeun-Oh <90241178+Daeun-Oh@users.noreply.github.com>
2025-06-07 18:33:21 +0900

    일주일 트렌드 - 꺾은선 그래프

--- secondhand/src/main/java/org/koreait/trend/repositories/TrendRepository.java
+++ secondhand/src/main/java/org/koreait/trend/repositories/TrendRepository.java
@@ -5,9 +5,23 @@ import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.ListCrudRepository;
 import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 
 public interface TrendRepository extends ListCrudRepository<Trend, Long> {
     @Query("SELECT * FROM TREND WHERE category=:category ORDER BY createdAt DESC LIMIT 1")
     Optional<Trend> getLatest(@Param("category") String category);
+
+    @Query(value = """
+    SELECT * FROM TREND
+    WHERE category = :category
+      AND DATE(createdAt) BETWEEN :sDate AND :eDate
+    ORDER BY createdAt ASC
+    """)
+    List<Trend> getList(
+            @Param("category") String category,
+            @Param("sDate") LocalDate sDate,
+            @Param("eDate") LocalDate eDate
+    );
 }
